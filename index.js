abbac40db51b62ae5e5dc46cdf522640b4bc652f
lakshdip patidar <lakshdippatidar@gmail.com>
2022-07-01 16:07:17 +0530

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+for (var i=0 ; i < (document.querySelectorAll("button").length) ; i++){
+
+    document.querySelectorAll("button")[i].addEventListener("click", function(){
+    alert("boof");
+    
+});
+}
+// 
+    // function drumBeat() {
+       
+    //     var audio = new Audio('sounds\tom-1.mp3');
+    //     audio.play(); 
+        
+    // }
